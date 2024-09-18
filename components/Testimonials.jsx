4535fcefa7faac8ef317b93192f8fdc639a7d7a8
Denis Bonaccini <dbonaccini8@gmail.com>
2024-09-18 13:09:04 -0300

    main

--- components/Testimonials.jsx
+++ components/Testimonials.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import Image from 'next/image';
 import { useState, useEffect } from 'react';
 import { FaStar } from 'react-icons/fa';
@@ -44,48 +44,48 @@ export default function Testimonials() {
 
   return (
     <div className="flex flex-wrap justify-center gap-8 my-8">
-    {displayTestimonials.map((testimonial, index) => (
-      <div
-        key={index}
-        className="relative bg-white p-6 rounded-3xl shadow-lg max-w-xs md:max-w-xl w-full text-center transition-transform transform hover:scale-105 hover:shadow-2xl"
-        style={{
-          backgroundImage: 'url("/texture.png")', // URL da textura
-          backgroundSize: 'cover',  // Ajusta a textura para cobrir o card
-          backgroundBlendMode: 'multiply',  // Multiplica a textura com a cor de fundo
-        }}
-      >
-        {/* Estilização com gradiente ao fundo */}
-        <div className="absolute inset-0 bg-gradient-to-br from-yellow-50 to-gray-100 rounded-3xl opacity-30"></div>
-        
-        <div className="relative z-10">
-          <div className="flex justify-center mb-4">
-            <Image
-              src={testimonial.image}
-              alt={`Foto de ${testimonial.name}`}
-              width={100}
-              height={100}
-              className="rounded-full border-4 border-yellow-500 shadow-lg"
-            />
-          </div>
-
-          {/* Estrelas de avaliação */}
-          <div className="flex justify-center mb-3 text-yellow-400">
-            {[...Array(5)].map((_, i) => (
-              <FaStar
-                key={i}
-                className="text-yellow-400 mr-1 transition-transform transform hover:scale-125"
+      {displayTestimonials.map((testimonial, index) => (
+        <div
+          key={index}
+          className="relative bg-white p-6 rounded-3xl shadow-lg max-w-xs md:max-w-xl w-full text-center transition-transform transform hover:scale-105 hover:shadow-2xl"
+          style={{
+            backgroundImage: 'url("/texture.png")', // URL da textura
+            backgroundSize: 'cover',  // Ajusta a textura para cobrir o card
+            backgroundBlendMode: 'multiply',  // Multiplica a textura com a cor de fundo
+          }}
+        >
+          {/* Estilização com gradiente ao fundo */}
+          <div className="absolute inset-0 bg-gradient-to-br from-yellow-50 to-gray-100 rounded-3xl opacity-30"></div>
+          
+          <div className="relative z-10">
+            <div className="flex justify-center mb-4">
+              <Image
+                src={testimonial.image}
+                alt={`Foto de ${testimonial.name}`}
+                width={100}
+                height={100}
+                className="rounded-full border-4 border-yellow-500 shadow-lg"
               />
-            ))}
-          </div>
+            </div>
 
-          {/* Depoimento */}
-          <p className="text-gray-700 mb-4 italic leading-relaxed mx-4 md:mx-8 lg:mx-8">
-            "{testimonial.text}"
-          </p>
-          <p className="font-semibold text-gray-900">- {testimonial.name}</p>
+            {/* Estrelas de avaliação */}
+            <div className="flex justify-center mb-3 text-yellow-400">
+              {[...Array(5)].map((_, i) => (
+                <FaStar
+                  key={i}
+                  className="text-yellow-400 mr-1 transition-transform transform hover:scale-125"
+                />
+              ))}
+            </div>
+
+            {/* Depoimento */}
+            <p className="text-gray-700 mb-4 italic leading-relaxed mx-4 md:mx-8 lg:mx-8">
+              "{testimonial.text}"
+            </p>
+            <p className="font-semibold text-gray-900">- {testimonial.name}</p>
+          </div>
         </div>
-      </div>
-    ))}
-  </div>
+      ))}
+    </div>
   );
 }
