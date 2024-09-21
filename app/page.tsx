d503c5e6daf4f3682b4b8440b3dfe1a7e6a93073
Denis Bonaccini <dbonaccini8@gmail.com>
2024-09-21 11:37:12 -0300

    video

--- app/page.tsx
+++ app/page.tsx
@@ -1,57 +1,85 @@
-"use client"
-import Image from "next/image";
-import { FaSortDown } from 'react-icons/fa';
+"use client";
+import { useState, useEffect, useRef } from 'react';
+import Head from 'next/head';
+import Headline from '../components/Headline';
+import About from '../components/About';
+import FAQAccordion from '../components/FAQAccordion';
+import Details from '../components/Details';
+import Testimonials from '../components/Testimonials';
+import Spacebar from '../components/Spacebar';
+import Element from '../components/Element';
+import WhatsappButton from '../components/WhatsappButton';
+import Footer from '@/components/Footer';
+import ExperienceDetails from '@/components/ExperienceDetails';
+import Loader from '../components/Loader'; // Importa o loader
 
-export default function Headline({ videoRef }: { videoRef: React.RefObject<HTMLVideoElement> }) {
-  
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+  const videoRef = useRef<HTMLVideoElement>(null); // Cria a referência do vídeo
+
+  // Usamos um efeito para verificar quando todos os recursos da página foram carregados
+  useEffect(() => {
+    const handlePageLoad = () => {
+      setIsLoading(false); // Quando a página estiver carregada, o loader desaparece
+
+      // Tenta reproduzir o vídeo automaticamente após o carregamento
+      if (videoRef.current) {
+        videoRef.current.play().catch((error) => {
+          console.error("Erro ao tentar reproduzir o vídeo automaticamente:", error);
+        });
+      }
+    };
+
+    // Quando a página está completamente carregada
+    if (document.readyState === 'complete') {
+      handlePageLoad();
+    } else {
+      window.addEventListener('load', handlePageLoad); // Ouve o evento 'load' para garantir que todos os recursos sejam carregados
+    }
+
+    // Limpa o event listener
+    return () => window.removeEventListener('load', handlePageLoad);
+  }, []);
+
   return (
-    <main className="relative flex flex-col items-center justify-center h-screen text-center px-4 md:px-8 lg:px-16">
-      
-      {/* Vídeo de fundo */}
-      <video
-        ref={videoRef} // Atribui a ref ao vídeo
-        className="absolute top-0 left-0 w-full h-full object-cover z-0"
-        autoPlay
-        loop
-        muted
-        playsInline
-        disablePictureInPicture
-      >
-        <source src="/videos/fundo.mp4" type="video/mp4" />
-        Seu navegador não suporta a tag de vídeo.
-      </video>
+    <div className="min-h-screen bg-tantra-dark overflow-hidden">
+      <Head>
+        <title>Home | Experiência Tantrica</title>
+        <meta name="description" content="A melhor experiência tantrica para seu crescimento pessoal e espiritual" />
+        <meta name="keywords" content="experiência tântrica, tantra, meditação, crescimento pessoal, sexualidade, ejaculação precoce, orgasmo, despertar sexual," />
+        <meta property="og:title" content="Experiência Tantrica" />
+        <meta property="og:description" content="A melhor experiência tantrica para seu crescimento pessoal e espiritual" />
+        <meta property="og:image" content="https://i.ibb.co/2nwWRsJ/Capa-Icone-Landing-Page.png" />
+        <meta property="og:url" content="https://daiane-grison.vercel.app" />
+      </Head>
 
-      <div className="relative z-20 my-1">
-        <Image
-          src="/logo.png"
-          alt="Chakras"
-          className=" mx-auto"
-          width={310} 
-          height={310}
-        />
-      </div>
-      
-      <div className="relative z-30">
-        <p className="my-8 font-light text-white text-lg md:text-xl lg:text-xl">
-          <span className="text-3xl text-dourado font-bold bg-opacity-30 bg-black py-1 px-2 rounded-md border-2 border-dourado">28/09</span>
-        </p>
-        <h1 className="text-xl md:text-3xl lg:text-3xl font-bold text-tantra-light max-w-4xl mx-3 uppercase">
-          Experiência Tântrica
-        </h1>
-        
-        <h2 className="mt-6 font-extralight text-lg md:text-2xl lg:text-2xl text-tantra-light max-w-3xl mx-6">
-          Desperte Seu Prazer, Conecte-se com sua Essência e Viva Plenamente
-        </h2>
-      </div>
-
-      <button
-        className="text-slate-300 mt-10 t font-semibold underline hover:text-white transition-colors duration-300"
-        onClick={() => {
-          document.getElementById('1')?.scrollIntoView({ behavior: 'smooth' });
-        }}
-      >
-        <FaSortDown className="flex animate-bounce text-white size-8 mt-20"/>
-      </button>
-    </main>
+      {/* Exibe o loader enquanto está carregando */}
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          {/* Passa o videoRef para o componente Headline */}
+          <Headline videoRef={videoRef} />
+          <Spacebar />
+          <ExperienceDetails />
+          <Spacebar />
+          <About />
+          <Spacebar />
+          <Element />
+          <Spacebar />
+          <Details />
+          <Spacebar />
+          <Testimonials />
+          <Spacebar />
+          <FAQAccordion />
+          <Spacebar />
+          <Footer />
+          <WhatsappButton />
+        </>
+      )}
+    </div>
   );
 }
